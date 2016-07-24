580c00235fab15cdea1a7d98749de9f5e8df0a7f
Dave Barthly <davebarthly@gmail.com>
2016-07-24 15:20:22 -0400

    Fix enemy not going to position on first millisecond

--- main.js
+++ main.js
@@ -1,99 +1,114 @@
 "use strict";
-(function(){
-$(function(){
+(function() {
+$(function() {
 // Start of code
 // file:///Users/satiewaltz/code/project1/index.html
 
   // Game object
   var game = {
-    allEnemies: $(".enemy"),
+
+    // Contains all functions pertaining to enemies
+    enemyGameLogic: {
+      selectAllEnemies: $(".enemy"),
+
+      // Create enemy data to append to DOM element
+      CreateEnemyData: function() {
+        this.randomClassGenerator = "enemy" + String(Math.floor(Math.random() * 4) + 1);
+        this.randomIDgenerator = Math.round(Math.random() * 50000);
+        this.randomVerticalPosition = Math.floor(Math.random() * 300);
+        this.enemyHealth = Math.floor(Math.random() * 5) + 1;
+        this.spawnLocation = 0;
+      },
+
+      // Set the position of the enemy using the previous data
+      enemyPosition: function(identifier, verticalPosition, spawnLocation) {
+        // Repeat to intialize positon on first millisecond
+        $("#" + identifier).css({
+          "top": verticalPosition,
+          "left": spawnLocation += 0.5 / (1 * game.enemySpeedModifer)
+        });
+        var updatePosition = setInterval(function() {
+          $("#" + identifier).css({
+            "top": verticalPosition,
+            "left": spawnLocation += 0.5 / (1 * game.enemySpeedModifer)
+          });
+          if (spawnLocation >= (game.levelWidth - 100)) {
+            spawnLocation = (game.levelWidth - 100);
+            $("#" + identifier).remove();
+            // console.log("you lose")
+          }
+        }, 1);
+      },
+
+      // Append the enemy to the DOM. Along with it's data
+      appendEnemyToDOM: function() {
+        var newEnemyData = new this.CreateEnemyData();
+        var enemyDIV = $("<div>");
+        enemyDIV.data(newEnemyData);
+        enemyDIV.addClass('enemy');
+        enemyDIV.addClass(enemyDIV.data().randomClassGenerator);
+        enemyDIV.attr('id', enemyDIV.data().randomIDgenerator);
+        setInterval(function() {
+          enemyDIV.text(enemyDIV.data().enemyHealth);
+        }, 1);
+        if (this.selectAllEnemies.length === 20) {
+          setInterval(function() {
+            this.selectAllEnemies.last().remove();
+          }, 1);
+        }
+        setInterval(function() {
+          if (enemyDIV.data().enemyHealth <= 0) {
+            $("#" + enemyDIV.data().randomIDgenerator).effect("explode", {pieces: 5}, 500).remove();
+          }
+          if (enemyDIV.data().spawnLocation >= (this.levelWidth - 100)) {
+            this.selectAllEnemies.remove();
+            console.log("You lose!");
+          }
+        }, 1);
+        game.level.append(enemyDIV);
+        this.enemyPosition(enemyDIV.data().randomIDgenerator,
+          enemyDIV.data().randomVerticalPosition,
+          enemyDIV.data().spawnLocation);
+      }
+    },
+
     level: $(".level"),
     levelWidth: $(".level").width(),
+
     enemySpeedModifer: 1,
     gameSpeed: [600, 700, 800, 1000],
     loseCondition: 0,
-    prevTimeStamp: '',
-    currTimeStamp: '',
-
     // Update level width on window resize
-    updatelevelWidth: setInterval(function(){
+    updatelevelWidth: setInterval(function() {
       game.levelWidth = $(".level").width();
     }, 1),
-
     // Speed up enemy spawn rate every ten seconds
-    enemySpawnRateIncreaser: setInterval(function(){
-      game.gameSpeed.shift()
+    enemySpawnRateIncreaser: setInterval(function() {
+      game.gameSpeed.shift();
     }, 10000),
-
     // Speed up enemy move speed every second
-    enemyMoveSpeedIncreaser: setInterval(function(){
-      game.enemySpeedModifer /= 1.02
-    }, 1000)
-  }
+    enemyMoveSpeedIncreaser: setInterval(function() {
+      game.enemySpeedModifer /= 1.02;
+    }, 1000),
 
-  var enemyPosition = function(identifier, verticalPosition, spawnLocation){
-    var updatePosition = setInterval(function() {
-      $("#" + identifier).css({
-        "top": verticalPosition,
-        "left": spawnLocation += 0.5 / (1 * game.enemySpeedModifer)
-      });
-      if (spawnLocation >= (game.levelWidth - 100)) {
-        spawnLocation = (game.levelWidth - 100);
-        $("#" + identifier).remove();
-        // console.log("you lose")
-      }
-    }, 1)
-  }
+    prevTimeStamp: '',
+    currTimeStamp: '',
+  };
 
-  function Enemy() {
-    this.randomClassGenerator = "enemy" + String(Math.floor(Math.random() * 4) + 1);
-    this.randomIDgenerator = Math.round(Math.random() * 50000);
-    this.randomVerticalPosition = Math.floor(Math.random() * 300);
-    this.enemyHealth = Math.floor(Math.random() * 5) + 1;
-    this.spawnLocation = 0;
-    this.location = enemyPosition(this.randomIDgenerator, this.randomVerticalPosition, this.spawnLocation);
-  }
 
-  function createEnemy(){
-    var newEnemyData = new Enemy;
-    var enemyDIV = $("<div>");
-    enemyDIV.data(newEnemyData);
-    enemyDIV.addClass('enemy');
-    var enemyHP = enemyDIV.data().enemyHealth;
-    enemyDIV.attr('id', enemyDIV.data().randomIDgenerator);
-    enemyDIV.addClass(enemyDIV.data().randomClassGenerator);
-    setInterval(function(){
-      enemyDIV.text(enemyDIV.data().enemyHealth);
-    }, 1)
-    game.level.append(enemyDIV);
-    if (game.allEnemies.length === 20) {
-      setInterval(function(){
-        game.allEnemies.last().remove();
-      }, 1)
-    }
-    setInterval(function(){
-      if (enemyDIV.data().enemyHealth <= 0) {
-        $("#" + enemyDIV.data().randomIDgenerator).effect("explode", {pieces: 5}, 500).remove();
-      }
-      if (enemyDIV.data().spawnLocation === (game.levelWidth - 100)) {
-        game.allEnemies.remove();
-        console.log("You lose!");
-      }
-    }, 1)
-  }
 
-  createEnemy();
-  var gamePlaying = setInterval(function(){
-    createEnemy()
+  game.enemyGameLogic.appendEnemyToDOM();
+  var gamePlaying = setInterval(function() {
+      game.enemyGameLogic.appendEnemyToDOM();
   }, game.gameSpeed[0]);
 
   if (game.loseCondition === 20) {
-    game.allEnemies.remove();
+    game.selectAllEnemies.remove();
     clearInterval(gamePlaying);
   }
 
-  function gamestate() {
-    game.currgamestate = requestAnimationFrame(gamestate);
+  (function listenToGamepad() {
+    requestAnimationFrame(listenToGamepad);
     var gamePad = navigator.getGamepads()[0];
     game.currTimeStamp = gamePad.timestamp;
     // Comparing the state of the current timestamp
@@ -104,34 +119,32 @@ $(function(){
       // thank you asynchronous functions!
       // Test to see if jQuery element exists
       // https://learn.jquery.com/using-jquery-core/faq/how-do-i-test-whether-an-element-exists/
-      if (gamePad.buttons[0].pressed) {
-        if ($(".enemy1").length){
-          $(".enemy1").first().data().enemyHealth -= 1;
-        }
-        // $(".enemy1").first()
+
+      // Green Button
+      if (gamePad.buttons[0].pressed && $(".enemy1").length) {
+        $(".enemy1").first().data().enemyHealth -= 1;
       }
-      if (gamePad.buttons[1].pressed) {
-        if ($(".enemy2").length){
-          $(".enemy2").first().data().enemyHealth -= 1;
-        // $(".enemy2").first().effect("explode", {pieces: 5}, 500).remove();
-        }
+
+      // Red Button
+      if (gamePad.buttons[1].pressed && $(".enemy2").length) {
+        $(".enemy2").first().data().enemyHealth -= 1;
       }
-      if (gamePad.buttons[2].pressed) {
-        if ($(".enemy3").length){
-          $(".enemy3").first().data().enemyHealth -= 1;
-        // $(".enemy3").first().effect("explode", {pieces: 5}, 500).remove();
-        }
+
+      // Blue Button
+      if (gamePad.buttons[2].pressed && $(".enemy3").length) {
+        $(".enemy3").first().data().enemyHealth -= 1;
       }
-      if (gamePad.buttons[3].pressed) {
-        if ($(".enemy4").length){
-          $(".enemy4").first().data().enemyHealth -= 1;
-        // $(".enemy4").first().effect("explode", {pieces: 5}, 500).remove();
-        }
+
+      // Yellow Button
+      if (gamePad.buttons[3].pressed && $(".enemy4").length) {
+        $(".enemy4").first().data().enemyHealth -= 1;
       }
     }
+
+    // Reassign the previous timestamp as the current.
+    // To compare over again.
     game.prevTimeStamp = game.currTimeStamp;
-  }
-gamestate();
+  })();
 // End of code
-})
+});
 })();
